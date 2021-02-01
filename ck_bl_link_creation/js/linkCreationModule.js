6e7c4cc09804dbd9e2f1c8361c9be5b2b644d216
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2021-02-01 14:31:53 -0500

    ck bl link update

--- ck_bl_link_creation/js/linkCreationModule.js
+++ ck_bl_link_creation/js/linkCreationModule.js
@@ -1,14 +1,10 @@
-function linkWorker() {
-    writeLinks([...new Set(createLinks(readTextArea()))]);
-}
+const linkWorker = () => writeLinks([...new Set(createLinks(readTextArea()))]);
 
-function readTextArea() {
-    return document.getElementById("cardNames").value.split("\n");
-}
+cosnt =  readTextArea = () => document.getElementById("cardNames").value.split("\n");
 
 function createLinks(cardNameArr) {
-    var linkFirstHalf = '<a href="https://www.cardkingdom.com/purchasing/mtg_singles?filter%5Bsort%5D=price_desc&filter%5Bsearch%5D=mtg_advanced&filter%5Bname%5D=';
-    var linkLastHalf = '&filter%5Bcategory_id%5D=0&filter%5Bfoil%5D=1&filter%5Bnonfoil%5D=1&filter%5Bprice_op%5D=&filter%5Bprice%5D=&cacheBust='+curUnixTime()+'" target=_blank >';
+    const linkFirstHalf = '<a href="https://www.cardkingdom.com/purchasing/mtg_singles?filter%5Bsort%5D=price_desc&filter%5Bsearch%5D=mtg_advanced&filter%5Bname%5D=';
+    const linkLastHalf = '&filter%5Bcategory_id%5D=0&filter%5Bfoil%5D=1&filter%5Bnonfoil%5D=1&filter%5Bprice_op%5D=&filter%5Bprice%5D=&cacheBust='+curUnixTime()+'" target=_blank >';
     return cardNameArr.map( name => linkFirstHalf + name.replace(/\s/g, "%20") + linkLastHalf + name + "</a>" );
 }
 
@@ -21,6 +17,4 @@ async function writeLinks(linkArr) {
     pauseClear();
 }
 
-function pauseClear() {
-    document.getElementById("clearData").disabled = !document.getElementById("clearData").disabled;
-}
+const pauseClear = () => document.getElementById("clearData").disabled = !document.getElementById("clearData").disabled;
