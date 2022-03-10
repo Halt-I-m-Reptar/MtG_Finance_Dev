6a11ff595cff2e27d0733b63f5b3d9e13d920f18
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2022-03-10 14:56:10 -0500

    updated CK card array

--- ck_api_data/modules/filterModule.js
+++ ck_api_data/modules/filterModule.js
@@ -10,12 +10,7 @@ const cleanFilter = (filterList) => filterList.map(cardName => cardName.replace(
 
 const cleanCkCardName = (card) => card.replace(/\W/g,'').toLowerCase();
 
-/*
-    CK sorts cards by SKU and those SKUs are not in MtG Chronolical order
-    To "fix" this and enable easier data display, we sort the output display
-    alphabetically by card name.
- */
 const filterTable = (filterList) => {
-    const filteredCardList = ckData.filter( curCard => filterList.includes(cleanCkCardName(curCard.name) ));
-    displayData(filteredCardList.sort( (firstCard, secondCard) => (firstCard.name > secondCard.name) ? 1 : ((secondCard.name > firstCard.name) ? -1 : 0)));
+    const filteredCardList = filterList.filter( currentCard => ckData[currentCard] ).map( currentCard => ckData[currentCard] ).sort( (firstCard, secondCard) => (firstCard.name > secondCard.name) ? 1 : ((secondCard.name > firstCard.name) ? -1 : 0));
+    displayData(filteredCardList);
 }
