c1e03a433694533d8e99a1872991203c40c32d1d
Peter Creutzberger <pcreutzberger@gmail.com>
2022-04-14 00:47:38 -0400

    added default date parameter

--- tampermonkey_scripts/js/TCG_Player_Sales_Display_Data.js
+++ tampermonkey_scripts/js/TCG_Player_Sales_Display_Data.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TCG Player Sales Display Data
 // @namespace    https://www.tcgplayer.com/
-// @version      0.18
+// @version      0.19
 // @description  Remove obfuscation around TCG Player Sales Data
 // @author       Peter Creutzberger
 // @match        https://www.tcgplayer.com/product/*
@@ -201,7 +201,7 @@
 
     const getSaleDateDiff = (firstDate, secondDate) => (parseStrDate(firstDate) - parseStrDate(secondDate)) / daysInMilliseconds(1);
 
-    const daysInMilliseconds = (days) => 1000 * 60 * 60 * (24 * days);
+    const daysInMilliseconds = (days = 1) => 1000 * 60 * 60 * (24 * days);
 
     const daysToLookBack = 2;
 
