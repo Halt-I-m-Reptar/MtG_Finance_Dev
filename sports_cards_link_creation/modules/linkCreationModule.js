4909160a09b7723a4def556beb2b0700feea340e
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2022-10-18 15:37:12 -0400

    data is now sorted

--- sports_cards_link_creation/modules/linkCreationModule.js
+++ sports_cards_link_creation/modules/linkCreationModule.js
@@ -1,28 +1,39 @@
 const linkWorker = () => {
     createTable();
-    writeLinks(readTextArea());
+    tableWorker(readTextArea());
 }
 
-const readTextArea = () => document.getElementById("productNames").value.split("\n");
+const readTextArea = () => document.getElementById("productNames").value.length ? document.getElementById("productNames").value.split("\n").sort() : false;
 
 const createTable = () => document.getElementById("listDisplay").innerHTML = '<table id="displayData" class="displayData"><thead><tr><th>Store Name</th><th>Product Name</th><th>Product URL</th></tr></thead><tbody id="productDisplayTable"></tbody></table>';
 
 const createLinkForDisplay = url  => `<a href="${url}" target="_blank">${url}</a>`;
 
-function writeLinks(productArr) {
-    const storeListObj = getStoreList();
+const errObject = () => ({0: "<h1>Please enter a list of products to search.</h1>"});
+
+function writeTableData(storeKey, storeListObj, productName) {
     const table = document.getElementById("displayData");
     let cell;
     let row;
-    Object.keys( storeListObj ).forEach( storeKey => {
-        productArr.forEach( productName => {
-            row = table.insertRow();
-            cell = row.insertCell(0);
-            cell.innerHTML = `${storeKey}`;
-            cell = row.insertCell(1);
-            cell.innerHTML = `${productName}`;
-            cell = row.insertCell(2);
-            cell.innerHTML = createLinkForDisplay(`${storeListObj[storeKey].url}${productName}` );
+    row = table.insertRow();
+    cell = row.insertCell(0);
+    cell.innerHTML = `${storeKey}`;
+    cell = row.insertCell(1);
+    cell.innerHTML = `${productName}`;
+    cell = row.insertCell(2);
+    cell.innerHTML = createLinkForDisplay(`${storeListObj[storeKey].url}${productName}` );
+}
+
+function tableWorker(productArr) {
+    try {
+        if (!productArr) { throw 0; }
+        const storeListObj = getStoreList();
+        productArr.forEach(productName => {
+            Object.keys(storeListObj).forEach(storeKey => {
+                writeTableData(storeKey, storeListObj, productName)
+            });
         });
-    });
+    } catch (err) {
+        document.getElementById("listDisplay").innerHTML = errObject()[err];
+    }
 }
