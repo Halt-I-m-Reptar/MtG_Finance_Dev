373e3d9b3fb7182602001daa2ea678ef4ae0be7d
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2022-12-06 10:53:14 -0500

    clened up some of the code in an efficiency effort

--- scg_bl_prices/js/genericFunctions.js
+++ scg_bl_prices/js/genericFunctions.js
@@ -1,5 +1,7 @@
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+const writeError = (error, cardName) => document.getElementById("displayData").innerHTML = `${error}<br />${cardName} was not found.`;
+
 const clearData = () => {
     document.getElementById("cardNames").value = '';
     document.getElementById("listDisplay").innerHTML = '';
