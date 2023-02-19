9d112c677944f42cb6a2f9174b636519758b2c6d
pcreutzberger <pcreutzberger@gmail.com>
2023-02-19 14:15:02 -0500

    initial updates and slug download

--- ck_api_data/modules/displayModule.js
+++ ck_api_data/modules/displayModule.js
@@ -25,18 +25,22 @@ const displayData = (ckData) => {
     writeToTable(ckData);
 }
 
-const createTable = () => document.getElementById("listDisplay").innerHTML = '<table id="displayData" class="displayData"><thead><tr><th>CK Id</th><th>SKU</th><th>URL</th><th>Card Name</th><th>Variation</th><th>Set</th><th>Foil</th><th>Retail Price</th><th>Retail Quantity</th><th>Buy Price</th><th>Buy Quantity</th></tr></thead><tbody id="cardDisplayTable"></tbody></table>';
+const createTable = () => document.getElementById("listDisplay").innerHTML = '<table id="displayData" class="displayData"><thead><tr><th>CK Id</th><th>SKU</th><th>URL</th><th>Card Name</th><th>Variation</th><th>Set</th><th>Foil</th><th>Retail Price</th><th>Retail Quantity</th><th>Buy Price</th><th>Buy Quantity</th><th>Display</th></tr></thead><tbody id="cardDisplayTable"></tbody></table>';
 
 const writeToTable = (ckCardData) => {
     const table = document.getElementById("displayData");
     let cell;
     let row;
-    ckCardData.forEach( cardArray => {
+    console.table(ckCardData);
+    console.table( Object.keys(ckCardData[0]) );
+    /*ckCardData.forEach( cardArray => {
         cardArray.forEach( cardById => {
             row = table.insertRow();
             Object.keys(cardById).forEach( (cardDataKey, index) => {
                 cell = row.insertCell(index);
                 cell.innerHTML = cardById[cardDataKey];
+                //        cell = row.insertCell();
+                //         cell.innerHTML = "<button onclick='hideRow(tableRowIndex)'>Hide</button>";
                 switch (index) {
                     case 3:
                         cell.className = "cardName";
@@ -58,5 +62,5 @@ const writeToTable = (ckCardData) => {
                 }
             })
         });
-    });
+    });*/
 }
