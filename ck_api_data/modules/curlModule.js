705cf11364c6dd9d0025387cfd1088b5184110d2
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2023-02-20 13:41:33 -0500

    updates galore

--- ck_api_data/modules/curlModule.js
+++ ck_api_data/modules/curlModule.js
@@ -1,6 +1,6 @@
 const curlRequest = () => {
-    //const url = "https://api.cardkingdom.com/api/pricelist";
-    const url = "https://raw.githubusercontent.com/Halt-I-m-Reptar/MtG_Finance_Dev/master/ck_api_data/ck_slug/pricelist.json";
+    const url = "https://api.cardkingdom.com/api/pricelist";
+    //const url = "https://raw.githubusercontent.com/Halt-I-m-Reptar/MtG_Finance_Dev/master/ck_api_data/ck_slug/pricelist.json";
 
     const requestOptions = {
         method: 'GET',
@@ -9,6 +9,6 @@ const curlRequest = () => {
 
     fetch(url, requestOptions)
         .then(response => response.json())
-        .then(result => startDisplayOutput(result))
+        .then(result => setCKData(result))
         .catch(error => writeError(error));
 }
--- ck_api_data/modules/filterModule.js
+++ ck_api_data/modules/filterModule.js
@@ -8,9 +8,14 @@ const getFilters = () => document.getElementById("cardNames").value.split("\n");
 
 const cleanFilter = (filterList) => filterList.map(cardName => cardName.replace(/\W/g,'').toLowerCase());
 
-const cleanCkCardName = (card) => card.replace(/\W/g,'').toLowerCase();
-
 const filterTable = (filterList) => {
     const filteredCardList = filterList.reduce( (acc, currentCard) => acc = ckData[currentCard], [] );
     displayData(filteredCardList);
 }
+
+const cleanCkCardName = (card) => card.replace(/\W/g,'').toLowerCase();
+
+const createAndShapeCKData = (json) => json.data.forEach(data => {
+    ckData[cleanCkCardName(data.name)] = ckData[cleanCkCardName(data.name)] || [];
+    ckData[cleanCkCardName(data.name)][data.id] = data;
+} );
