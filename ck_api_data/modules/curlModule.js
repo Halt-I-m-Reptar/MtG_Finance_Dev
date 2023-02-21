ed0cbcdbd8c0f8dfb621533541c733266657e5b9
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2023-02-21 10:34:52 -0500

    whole-ass refactor

--- ck_api_data/modules/curlModule.js
+++ ck_api_data/modules/curlModule.js
@@ -1,5 +1,12 @@
+const jsonGetWorker = () => {
+    disableCKDataPull();
+    writeContentToDisplay(`Gathering and collating all inventory from CK.`);
+    displayLoadIcon();
+    curlRequest();
+}
+
 const curlRequest = () => {
-    const url = checkSlug() ? "https://api.cardkingdom.com/api/pricelist" : "https://raw.githubusercontent.com/Halt-I-m-Reptar/MtG_Finance_Dev/master/ck_api_data/ck_slug/pricelist.json";
+    const url = slugChoice() ? "https://api.cardkingdom.com/api/pricelist" : "https://raw.githubusercontent.com/Halt-I-m-Reptar/MtG_Finance_Dev/master/ck_api_data/ck_slug/pricelist.json";
 
     const requestOptions = {
         method: 'GET',
@@ -8,6 +15,6 @@ const curlRequest = () => {
 
     fetch(url, requestOptions)
         .then(response => response.json())
-        .then(result => setCKData(result))
+        .then(result => createCKDataSet(result))
         .catch(error => writeError(error));
 }
