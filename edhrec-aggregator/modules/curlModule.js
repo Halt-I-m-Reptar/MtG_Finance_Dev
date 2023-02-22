831543c80ae4cecbad26fde56a7ded5ed5fd0576
pcreutzberger <pcreutzberger@gmail.com>
2023-02-21 21:24:28 -0500

    added new example urls

--- edhrec-aggregator/modules/curlModule.js
+++ edhrec-aggregator/modules/curlModule.js
@@ -7,7 +7,12 @@ const jsonGetWorker = () => {
 
 const curlRequest = () => {
     //const url = slugChoice() ? "https://api.cardkingdom.com/api/pricelist" : "https://raw.githubusercontent.com/Halt-I-m-Reptar/MtG_Finance_Dev/master/ck_api_data/ck_slug/pricelist.json";
+    //card results
     const url = "https://json.edhrec.com/pages/commanders/sliver-queen.json";
+    //top cards per year
+    //https://json.edhrec.com/pages/top/year.json
+    // top commanders per year
+    // https://json.edhrec.com/pages/commanders/year.json
     const requestOptions = {
         method: 'GET',
         redirect: 'follow'
