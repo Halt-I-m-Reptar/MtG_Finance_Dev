5d55efe85dc85a4b561ac18b095190e92911ac9c
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2023-03-03 15:36:37 -0500

    individual card search in place

--- edhrec-aggregator/modules/inputWorkersModule.js
+++ edhrec-aggregator/modules/inputWorkersModule.js
@@ -4,6 +4,7 @@ const getCardColorValue = () => document.getElementById("topCardsByColor").value
 
 const getCommanderColorValue = () => document.getElementById("topCommandersByColor").value;
 
+//this is probably going to get messy
 const getTopCardValue = () => document.getElementById("topCardSearch").value;
 
 const getCardColorText = () => {
--- edhrec-aggregator/modules/topCard.js
+++ edhrec-aggregator/modules/topCard.js
@@ -1,3 +1,7 @@
 const getTopCardWorker = () => {
-    console.log( getTopCardValue() );
+    prepCurlRequest('cards', cleanInputCardName( getTopCardValue() ), 'topIndividualCards');
+    displayLoadIcon();
 }
+
+//This is going to get messy;
+const cleanInputCardName = (cardName) => cardName.replace(/'/g,'').replace(/\W+/gi,'-').toLowerCase();
