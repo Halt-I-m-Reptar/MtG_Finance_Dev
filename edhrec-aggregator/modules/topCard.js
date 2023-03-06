06135342163a6d1e9cfca53a97f4a1befba728b6
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2023-03-06 10:59:03 -0500

    individual card update

--- edhrec-aggregator/modules/topCard.js
+++ edhrec-aggregator/modules/topCard.js
@@ -1,7 +1,10 @@
+// the 'asCommander' data set is a list of cards played alongside the commander thus the standard cord processing
+// methods are applicable and the only demarcation we need is in the request url
 const getTopCardWorker = () => {
     const searchCardName = getTopCardValue();
+    const typeOfCardSearch = getElementValueQuerySelector('typeOfCardSearch') === 'asCard' ? 'cards' : 'commanders' ;
     prepDisplayDuringLoad( `cards played with ${searchCardName}` );
-    prepCurlRequest('cards', cleanInputCardName( searchCardName) , 'topIndividualCards');
+    prepCurlRequest(typeOfCardSearch, cleanInputCardName( searchCardName) , 'topIndividualCards');
 }
 
 //This is going to get messy;
