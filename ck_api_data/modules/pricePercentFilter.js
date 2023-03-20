c0d947502607c5514653b0e6ee690d05387d6cd6
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2023-03-20 16:14:21 -0400

    need to finalize this loop

--- ck_api_data/modules/pricePercentFilter.js
+++ ck_api_data/modules/pricePercentFilter.js
@@ -1,34 +1,31 @@
 const showHighPercentCards = () => {
     setListDomInnerHTML('listDisplay',"<strong>Gathering cards with a high buylist ratio. This may take some time.</strong>");
     displayLoadIcon();
-    const cardArr = filterCardsByPercent(Object.keys(ckCardDataFromSlug));
+    const cardArr = filterCardsByPercent();
     displayLoadIcon();
-    //createOutputTable();
-    //writeCardsToTable(cardArr);
+    createOutputTable();
+    writeCardsToTable(cardArr);
 }
 
-const filterCardsByPercent = (cardNameArray) => {
+const filterCardsByPercent = () => {
     const buylistPercentDiff = +getElementValueById('buylistPercentDiff');
-    const filteredCardList = [];
-    //const promises = [];
+    //const filteredCardList = [];
 
-    /*cardNameArray.forEach( cardName => {
-            setListDomInnerHTML('listDisplay',`<strong>Working on a card</strong>`);
+    /*Object.keys(ckCardDataFromSlug).forEach( cardName => {
+            setListDomInnerHTML('listDisplay',`<strong>Working on a card ${ckCardDataFromSlug[cardName]}</strong>`);
             ckCardDataFromSlug[cardName].forEach( currentCardById => {
                 if ( currentCardById['retailBuyPricePercent'] > buylistPercentDiff && currentCardById['qty_buying'] > 0 ) { filteredCardList.push( ckCardDataFromSlug[cardName][currentCardById['id']] ) }
             })
-    });*/
-
-    cardNameArray.map( cardName => ckCardDataFromSlug[cardName] ).filter( currentCardByName => {
-        //setListDomInnerHTML('listDisplay',`<strong>Working on a card</strong>`);
-        currentCardByName.forEach( currentCardById => {
-            if ( currentCardById['retailBuyPricePercent'] > buylistPercentDiff && currentCardById['qty_buying'] > 0) {
-                console.log(currentCardById['name']);
-                //filteredCardList.push( ckCardDataFromSlug[cardData['name']][cardData['id']] );
-            }
-        })
-        //setListDomInnerHTML('listDisplay',`<strong>Working on a card</strong>`);
-        //return ckCardDataFromSlug[cardName]
+    });
+     */
+    const filteredCardList = Object.keys(ckCardDataFromSlug).map( cardName => ckCardDataFromSlug[cardName] ).filter( currentCardById => {
+        console.log( currentCardById );
+        if ( currentCardById['retailBuyPricePercent'] > buylistPercentDiff && currentCardById['qty_buying'] > 0 ) { filteredCardList.push( ckCardDataFromSlug[currentCardById['name']][currentCardById['id']] ) }
+        return;
+        /*setListDomInnerHTML('listDisplay',`<strong>Working on a card ${ckCardDataFromSlug[cardName]}</strong>`);
+        ckCardDataFromSlug[cardName].forEach( currentCardById => {
+            if ( currentCardById['retailBuyPricePercent'] > buylistPercentDiff && currentCardById['qty_buying'] > 0 ) { filteredCardList.push( ckCardDataFromSlug[cardName][currentCardById['id']] ) }
+        })*/
     });
 
     console.log(filteredCardList);
