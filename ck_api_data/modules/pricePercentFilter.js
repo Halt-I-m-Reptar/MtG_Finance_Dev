1e432a300631280ffd717ac2ea950377c13bbe2e
peter.creutzberger <peter.creutzberger@coxautoinc.com>
2023-03-27 15:46:30 -0400

    updated functionality to read better

--- ck_api_data/modules/pricePercentFilter.js
+++ ck_api_data/modules/pricePercentFilter.js
@@ -2,22 +2,27 @@ const showHighPercentCards = async () => {
     displayLoadIcon();
     setListDomInnerHTML('listDisplay',"<strong>Gathering cards with a high buylist ratio. This may take some time.</strong>");
     const cardArr = await filterCardsByPercent();
-    displayLoadIcon();
-    createOutputTable();
-    writeCardsToTable(cardArr);
+    if ( cardArr.length ) {
+        displayLoadIcon();
+        createOutputTable();
+        writeCardsToTable(cardArr);
+    } else {
+        displayLoadIcon();
+        setListDomInnerHTML('listDisplay',"<strong>No cards matched your criteria. Try reducing the buylist percent difference.</strong>");
+    }
 }
 
 const filterCardsByPercent = async () => {
     const sortOption = getElementValueById('buylistPercentDiffSort');
+
     return await Object.values(ckCardDataFromSlug).map( currentCardById => currentCardById ).reduce( (acc, currentCard) => {
         Object.values(currentCard).filter( individualCard => {
             if ( individualCard['retailBuyPricePercent'] > +getElementValueById('buylistPercentDiff') && individualCard['qty_buying'] > 0 ) {
                 if ( getCheckedValue('showVariations') ) {
                     acc.push( individualCard );
-                } else {
-                    if ( !Boolean(individualCard['variation']) ) {
-                        acc.push( individualCard );
-                    }
+                }
+                if ( !getCheckedValue('showVariations') && !Boolean(individualCard['variation']) ) {
+                    acc.push( individualCard );
                 }
             }
         });
