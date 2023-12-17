53619d2cd9c2b99d79e31388006c7677996249ee
Peter Creutzberger <peter.creutzberger@coxautoinc.com>
2023-12-17 13:25:18 -0500

    set default filtering options for buylist sorting

--- ck_api_data/js/createDropDownMenus.js
+++ ck_api_data/js/createDropDownMenus.js
@@ -2,12 +2,12 @@
 
     const createBuyPricePercentDropDown = () => {
         const cardTypeMap = {
-            50: '50',
-            55: '55',
-            60: '60',
-            65: '65',
-            70: '70',
-            75: '75',
+            50: ['50', ''],
+            55: ['55', ''],
+            60: ['60', 'selected'],
+            65: ['65', ''],
+            70: ['70', ''],
+            75: ['75', ''],
         }
 
         appendItemsToSelect(getElementById("buylistPercentDiff"), cardTypeMap);
@@ -15,21 +15,22 @@
 
     const createBuyPricePercentSortOptions = () => {
         const sortOptionsMap = {
-            'retailBuyPricePercent': 'Buy Price Percent',
-            'price_retail': 'Retail Price',
-            'price_buy': 'Buy Price',
-            'qty_buying': 'Buy Quantity',
-            'qty_retail': 'Retail Quantity'
+            'retailBuyPricePercent': ['Buy Price Percent', ''],
+            'price_retail': ['Retail Price', ''],
+            'price_buy': ['Buy Price', ''],
+            'qty_buying': ['Buy Quantity', 'selected'],
+            'qty_retail': ['Retail Quantity', ''],
         }
 
         appendItemsToSelect(getElementById("buylistPercentDiffSort"), sortOptionsMap);
     }
 
     const appendItemsToSelect = (targetSelect, optionMap) => {
-        Object.keys(optionMap).forEach( selector => {
+        Object.keys(optionMap).forEach( selectorOptions => {
             const createOption = document.createElement('option');
-            createOption.value = selector;
-            createOption.innerHTML = optionMap[selector];
+            createOption.value = selectorOptions ;
+            createOption.innerHTML = optionMap[ selectorOptions ][0] ;
+            createOption.selected = optionMap[ selectorOptions ][1];
             targetSelect.appendChild(createOption);
         }) ;
     }
