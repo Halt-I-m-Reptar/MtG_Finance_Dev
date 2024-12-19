b78256dd69d777004feed2444562521234b32c42
Peter <pcreutzberger@gmail.com>
2024-12-19 18:56:29 -0500

    ready for alpha

--- mtg-spoiler-bot/main.py
+++ mtg-spoiler-bot/main.py
@@ -19,29 +19,27 @@ member: Member = Member
 async def schedule_spoiler_search():
     from modules.output_to_discord import send_message_to_discord
 
-    while True:
-        current_time = datetime.now()
-        #next_run = current_time + timedelta(hours=1)
-        next_run = current_time + timedelta(seconds=10)
-        wait_time = ( next_run - current_time ).total_seconds()
+    current_time = datetime.now()
+    next_run = current_time + timedelta(seconds=10)
+    wait_time = ( next_run - current_time ).total_seconds()
 
-        await asyncio.sleep( wait_time )
+    await asyncio.sleep( wait_time )
 
-        set_name_data, card_data_object = bot_main()
+    set_name_data, card_data_object = bot_main()
 
-        if card_data_object:
-            await write_card_data_to_file( set_name_data, card_data_object )
-            await send_message_to_discord( client, card_data_object )
+    if card_data_object:
+        await write_card_data_to_file( set_name_data, card_data_object )
+        await send_message_to_discord( client, card_data_object )
 
 
 @client.event
 async def on_ready():
     await schedule_spoiler_search()
+    await client.close()
 
 
 def main():
     client.run(token=TOKEN)
 
 
-if __name__ == '__main__':
-    main()
+main()
