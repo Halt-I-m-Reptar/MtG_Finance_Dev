6c585aa400dcfdc0faa5c143fb155b249252e381
Peter <pcreutzberger@gmail.com>
2025-04-09 22:06:23 -0400

    spacing updates

--- sports_cards_link_creation/modules/linkCreationModule.js
+++ sports_cards_link_creation/modules/linkCreationModule.js
@@ -1,6 +1,6 @@
-const readTextArea = () => document.getElementById("productNames" ).value.length ? document.getElementById("productNames" ).value.split( "\n" ).sort() : null;
+const readTextArea = () => document.getElementById( "productNames" ).value.length ? document.getElementById( "productNames" ).value.split( "\n" ).sort() : null;
 
-const createTable = () => document.getElementById("listDisplay" ).innerHTML = '<table id="displayData" class="displayData"><thead><tr><th>Store Name</th><th>Product Name</th><th>Product URL</th></tr></thead><tbody id="productDisplayTable"></tbody></table>';
+const createTable = () => document.getElementById( "listDisplay" ).innerHTML = '<table id="displayData" class="displayData"><thead><tr><th>Store Name</th><th>Product Name</th><th>Product URL</th></tr></thead><tbody id="productDisplayTable"></tbody></table>';
 
 const createLinkForDisplay = url => `<a href="${ url }" target="_blank">${ url }</a>`;
 
@@ -10,7 +10,7 @@ function linkWorker() {
         if ( !productArr ) { throw 0; }
         tableWorker( productArr );
     } catch ( err ) {
-        document.getElementById("listDisplay" ).innerHTML = errObject()[ err ];
+        document.getElementById( "listDisplay" ).innerHTML = errObject()[ err ];
     }
 }
 
@@ -25,7 +25,7 @@ function tableWorker( productArr ) {
 }
 
 function writeTableData( displayDataArr ) {
-    const table = document.getElementById("displayData");
+    const table = document.getElementById( "displayData");
     let cell;
     let row;
     row = table.insertRow();
