516d32e5f8a64050d7a5ede2535a95d964c76033
Peter Creutzberger <Peter.Creutzberger@coxautoinc.com>
2025-09-04 17:13:41 -0400

    eod push

--- mtgcc-spoiler-bot/modules/set_release_dates/check_if_exists.py
+++ mtgcc-spoiler-bot/modules/set_release_dates/check_if_exists.py
@@ -13,11 +13,28 @@ def check_if_set_exists( set_release_dates_from_url ):
 
     for set_release_data in set_release_dates_from_url:
         set_name_raw = set_release_data.split(': ')
-        set_name_clean = clean_set_name(set_name_raw[0])
-        if set_name_clean not in set_data_from_file:
-            data_to_write.append( f'{set_name_clean}: {set_name_raw[1]}' )
+
+        set_name = return_clean_set_name( set_name_raw )
+        set_date = set_name_raw[-1:][0]
+
+        if set_name not in set_data_from_file:
+            data_to_write.append( f'{set_name}: {set_date}' )
 
     return data_to_write
+
+
+#Did not account for sets that use a colon in the name and now need to merge the name parts back together
+def return_clean_set_name( set_name_raw ):
+    set_name_clean = clean_set_name(set_name_raw[0])
+
+    if len(set_name_raw) > 2:
+        set_name_clean = clean_set_name ( merge_names( set_name_raw ) )
+
+    return set_name_clean
+
+
+def merge_names( set_name_parts ):
+    return ': '.join(set_name_parts[:-1])
 
 
 def clean_set_name( set_name ):
