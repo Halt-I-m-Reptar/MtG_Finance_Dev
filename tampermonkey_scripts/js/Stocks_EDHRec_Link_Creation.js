978fdf54f44aceaf33cc9e61dc651a4e272df346
Peter <pcreutzberger@gmail.com>
2026-02-07 17:27:01 -0500

    css change at stocks

--- tampermonkey_scripts/js/Stocks_EDHRec_Link_Creation.js
+++ tampermonkey_scripts/js/Stocks_EDHRec_Link_Creation.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         MtG Stock EDH Rec Link Addition
-// @version      0.1
+// @version      0.02
 // @description  Add link to EDHRec page of the card being viewed
 // @author       Peter Creutzberger
 // @match        *://*.mtgstocks.com/prints/*
 // @grant        none
 // @namespace https://www.mtgstocks.com/
+// @downloadURL https://update.greasyfork.org/scripts/501604/MtG%20Stock%20EDH%20Rec%20Link%20Addition.user.js
+// @updateURL https://update.greasyfork.org/scripts/501604/MtG%20Stock%20EDH%20Rec%20Link%20Addition.meta.js
 // ==/UserScript==
 
 (function() {
@@ -15,8 +17,8 @@
 
     function addEDHRecLink() {
         console.log('adding link');
-        const cleanedName = document.getElementsByClassName('float-start')[0].innerText.split(' (')[0].replace(/\W+/g, '-').toLowerCase();
-        document.getElementsByClassName('float-start')[0].innerHTML += `\t--\t<a href='https://edhrec.com/cards/${cleanedName}' target='_blank'>EDH Rec Link</a>`;
+        const cleanedName = document.getElementsByClassName('content-title')[0].innerText.split("\n")[0].replace(/\W+/g, '-').toLowerCase();
+        document.getElementsByClassName('content-title')[0].innerHTML += `\t--\t<a href='https://edhrec.com/cards/${cleanedName}' target='_blank'>EDH Rec Link</a>`;
     }
 
 })();
