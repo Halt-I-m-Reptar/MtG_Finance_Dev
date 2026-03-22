76bf40a7210d146deafac7486b760d4acf7bd017
Peter <pcreutzberger@gmail.com>
2026-03-21 21:37:24 -0400

    spacing update

--- tampermonkey_scripts/js/Stocks_EDHRec_Link_Creation.js
+++ tampermonkey_scripts/js/Stocks_EDHRec_Link_Creation.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MtG Stock EDH Rec Link Addition
-// @version      0.02
+// @version      0.03
 // @description  Add link to EDHRec page of the card being viewed
 // @author       Peter Creutzberger
 // @match        *://*.mtgstocks.com/prints/*
@@ -18,7 +18,7 @@
     function addEDHRecLink() {
         console.log('adding link');
         const cleanedName = document.getElementsByClassName('content-title')[0].innerText.split("\n")[0].replace(/\W+/g, '-').toLowerCase();
-        document.getElementsByClassName('content-title')[0].innerHTML += `\t--\t<a href='https://edhrec.com/cards/${cleanedName}' target='_blank'>EDH Rec Link</a>`;
+        document.getElementsByClassName('content-title')[0].innerHTML += `\t<a href='https://edhrec.com/cards/${cleanedName}' target='_blank'>EDH Rec Link</a>`;
     }
 
 })();
